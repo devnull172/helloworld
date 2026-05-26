9068825b15d8dab6b43ee9fa40940cb0db8fc1b5
Dikshita Bhowmik <dikshita.bhowmik@gmail.com>
2026-05-25 22:38:03 -0400

    new

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world"
+    return "hello world <h1>hello world</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
